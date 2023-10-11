ee6852574414952f32982af7d0390c7b8bb7a96b
camilo <cjumelle@alumnat.copernic.cat>
2023-10-11 17:06:05 +0200

    Borrado archivos dobles

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'db_host' => 'localhost', // Dirección IP o nombre del host de la base de datos
-    'db_port' => 3306,        // Puerto de la base de datos
-    'db_name' => 'slidescarmonagalindojumelle', // Nombre de la base de datos
-    'db_user' => 'root',      // Nombre de usuario de la base de datos
-    'db_pass' => '1234',      // Contraseña de la base de datos
-];
-?>
