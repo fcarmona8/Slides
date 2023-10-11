9b5864194062434587372510203ec8d02457766c
camilo <cjumelle@alumnat.copernic.cat>
2023-10-11 20:15:39 +0200

    Editado archivos sobre la conexion a vagrant

--- codigo/index.php
+++ codigo/index.php
@@ -1,26 +1,31 @@
 <?php
-// Incluir el archivo de configuración
-$config = include('config.php');
+// Si se usa el archivo config descomentar y comentar las variables de abajo
+// $config = include('config.php');
 
-// Valores de configuración
-$db_host = $config['db_host'];
-$db_port = $config['db_port'];
-$db_name = $config['db_name'];
-$db_user = $config['db_user'];
-$db_pass = $config['db_pass'];
+// $db_host = $config['db_host'];
+// $db_port = $config['db_port'];
+// $db_name = $config['db_name'];
+// $db_user = $config['db_user'];
+// $db_pass = $config['db_pass'];
+
+$db_host = '192.168.1.12';
+$db_name = 'slidescarmonagalindojumelle';
+$db_user = 'root';
+$db_pass = 1234;
+$db_port = 3306;
+
 
 try {
     // Intenta establecer la conexión a la base de datos
-    $conn = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_pass);
+    $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
     // Configura el modo de errores y atributos de PDO según sea necesario
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Realiza una consulta simple para verificar la conexión
-    $stmt = $conn->query('SELECT 1');
     
     // Si la consulta se ejecuta sin errores, la conexión se ha establecido correctamente
     echo 'Conexión exitosa.';
 } catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
+    var_dump($e);
 }
+
+$conn = null;
 ?>
---
+++ vagrant/script.sh
@@ -0,0 +1,21 @@
+
+    apt-get update
+    
+    DBPASS=1234
+    DBNAME=slidescarmonagalindojumelle
+
+
+    sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASS"
+    sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASS"
+
+    apt-get install -y mysql-server    
+    mysql -u root -p$DBPASS -e "GRANT ALL PRIVILEGES ON * . * TO 'root' IDENTIFIED BY '1234';"
+    mysql -u root -p$DBPASS -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';"
+    mysql -u root -p$DBPASS -e "CREATE DATABASE $DBNAME;"
+    mysql -u root -p$DBPASS -e "USE $DBNAME; FLUSH PRIVILEGES;"
+    mysql -u root -p$DBPASS -e "USE $DBNAME; CREATE TABLE Presentacions (ID_Presentacio INT AUTO_INCREMENT PRIMARY KEY, titol VARCHAR(255) NOT NULL, descripcio TEXT);"
+    mysql -u root -p$DBPASS -e "USE $DBNAME; CREATE TABLE Diapositives (ID_Diapositiva INT AUTO_INCREMENT PRIMARY KEY, titol VARCHAR(255) NOT NULL, contingut TEXT);"
+    mysql -u root -p$DBPASS -e "USE $DBNAME; CREATE TABLE Presentacions_Diapositives (ID_Presentacio INT, ID_Diapositiva INT, PRIMARY KEY (ID_Presentacio, ID_Diapositiva), FOREIGN KEY (ID_Presentacio) REFERENCES Presentacions(ID_Presentacio), FOREIGN KEY (ID_Diapositiva) REFERENCES Diapositives(ID_Diapositiva));"
+
+    sed -i 's/127.0.0.1/0.0.0.0/g'  /etc/mysql/my.cnf
+    service mysql restart
