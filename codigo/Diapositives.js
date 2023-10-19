5eecc989e0691398c45e8cb9326aece4c5685da2
Ferran Carmona <ferrancarmona2000@gmail.com>
2023-10-19 20:46:36 +0200

    Funcionalidad para reordenar usando drag and drop diapositivas agregada

--- codigo/Diapositives.js
+++ codigo/Diapositives.js
@@ -11,21 +11,29 @@ button.addEventListener('click', function (e) {
     document.location.href = 'Home.php';
 });
 
-
 const mostrarDiapositivaColumna = () => {
-    const newVisualDiapo = document.createElement('div');
-    if (diapositives && diapositives.length > contadorArray) {
-        const displayTitle = diapositives[contadorArray][0] || 'Sin título';
+  const newVisualDiapo = document.createElement('div');
+  if (diapositives && diapositives.length > contadorArray) {
+      const displayTitle = diapositives[contadorArray][0] || 'Sin título';
 
-        const text = document.createTextNode(displayTitle);
-        newVisualDiapo.appendChild(text);
-        newVisualDiapo.classList.add('diaposInfo');
-        visualizarDiapo.insertAdjacentElement('beforeend', newVisualDiapo);
-        contadorArray++;
-        titol.value = null;
-        contingut.value = null;
-        titolDiapo = '';
-    }
+      const text = document.createTextNode(displayTitle);
+      newVisualDiapo.appendChild(text);
+      newVisualDiapo.classList.add('diaposInfo');
+      visualizarDiapo.insertAdjacentElement('beforeend', newVisualDiapo);
+
+      // Agregar atributo draggable para habilitar arrastrar para reordenar
+      newVisualDiapo.setAttribute('draggable', true);
+
+      // Agregar eventos para reordenar diapositivas
+      newVisualDiapo.addEventListener('dragstart', dragStart);
+      newVisualDiapo.addEventListener('dragover', dragOver);
+      newVisualDiapo.addEventListener('drop', drop);
+
+      contadorArray++;
+      titol.value = null;
+      contingut.value = null;
+      titolDiapo = '';
+  }
 };
 
 newDiapo.addEventListener('change', function (e) {
@@ -92,3 +100,33 @@ newDiapo.addEventListener('change', function (e) {
         }
     });
 });
+
+// Funciones Drag and Drop para reordenar las diapositivas
+function dragStart(e) {
+  e.dataTransfer.setData('text/plain', this.textContent);
+  this.classList.add('dragging');
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  
+}
+
+function drop(e) {
+  e.preventDefault();
+  const draggedElement = document.querySelector('.dragging');
+  const indexFrom = Array.from(visualizarDiapo.children).indexOf(draggedElement);
+  const indexTo = Array.from(visualizarDiapo.children).indexOf(this);
+
+  [diapositives[indexFrom], diapositives[indexTo]] = [diapositives[indexTo], diapositives[indexFrom]];
+
+  if (indexFrom < indexTo) {
+    visualizarDiapo.insertBefore(draggedElement, this.nextSibling);
+  } else {
+    visualizarDiapo.insertBefore(draggedElement, this);
+  }
+
+  draggedElement.classList.remove('dragging');
+  console.log(diapositives);
+}
+
