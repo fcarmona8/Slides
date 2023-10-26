63fdfdbc6d1841016b086b6c57dd9e17f55f5e89
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2023-10-26 20:13:25 +0200

    arreglat color text preview diapo

--- codigo/previsualitzarDiapositiva.php
+++ codigo/previsualitzarDiapositiva.php
@@ -14,7 +14,8 @@ $id_presentacio = isset($_GET["id"]) ? $_GET["id"] : "";
         <button class="boton-salir" id="volverButton"><svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg></button>
     </div>
     <div class="aviso">
-    <p id="aviso" style="color: red;"></p>
+        <p id="aviso" style="color: red;"></p>
+    </div>
     <div>
     <div class="diapositiva-preview">
         <h1 id="tituloPrevisualizado"></h1>
