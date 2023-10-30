6ace6fc723cda5753ca9616dd75370f6d39e02ad
Ferran Carmona <ferrancarmona2000@gmail.com>
2023-10-30 16:29:38 +0100

    eliminacio de codi sense utilitat i actualitzacio d'estils

--- codigo/Diapositives.js
+++ codigo/Diapositives.js
@@ -1,35 +1,3 @@
-const visualizarDiapo = document.querySelector('.diapositivas');
-const newDiapo = document.getElementById('tipus');
-const añadirDiapositiva = document.getElementById('añadirDiapositiva');
-
-let diapositives = []; //array [titol, cont][titol, null][titol,cont]
-let titol = '', contingut = '', titolContingut = '', titolDiapo = '';
-let contadorArray = 0;
-
-const mostrarDiapositivaColumna = () => {
-  const newVisualDiapo = document.createElement('div');
-  if (diapositives && diapositives.length > contadorArray) {
-      const displayTitle = diapositives[contadorArray][0] || 'Sin título';
-
-      const text = document.createTextNode(displayTitle);
-      newVisualDiapo.appendChild(text);
-      newVisualDiapo.classList.add('diaposInfo');
-      visualizarDiapo.insertAdjacentElement('beforeend', newVisualDiapo);
-
-      // Agregar atributo draggable para habilitar arrastrar para reordenar
-      newVisualDiapo.setAttribute('draggable', true);
-
-      // Agregar eventos para reordenar diapositivas
-      newVisualDiapo.addEventListener('dragstart', dragStart);
-      newVisualDiapo.addEventListener('dragover', dragOver);
-      newVisualDiapo.addEventListener('drop', drop);
-
-      contadorArray++;
-      titol.value = null;
-      contingut.value = null;
-      titolDiapo = '';
-  }
-};
 
 // Función para mostrar el mensaje durante 3 segundos y luego ocultarlo
 function mostrarMensajeExito() {
