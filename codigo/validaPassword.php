1e674e3d2bcc6ec8dd4e3aaba881667f1ec94c5a
Ferran Carmona <ferrancarmona2000@gmail.com>
2023-10-31 20:40:50 +0100

    actualitzacio pagina validaPassword per facilitar l'us per l'usuari

--- codigo/validaPassword.php
+++ codigo/validaPassword.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("Location: vistaPreviaClient.php?id=$id_presentacio");
         exit();
     } else {
-
+        $error_message = "PIN incorrecte";
     }
 }
 ?>
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <h2>Introdueix el PIN</h2>
         <form method="post" class="verificarPinForm">
                 <div class="passwordFieldContainer">
-                    <input type="password" id="password" name="password" class="" placeholder="PIN"
+                    <input type="password" id="password" name="password" class="<?php echo isset($error_message) ? 'errorPin ' : ' '; ?>" placeholder="PIN"
                         maxlength="20"></input>
                     <svg class="eye-icon" id="togglePassword" xmlns="http://www.w3.org/2000/svg" height="1em"
                         viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
@@ -58,6 +58,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <path
                             d="M288 80c-65.2 0-118.8 29.6-159.9 67.7C89.6 183.5 63 226 49.4 256c13.6 30 40.2 72.5 78.6 108.3C169.2 402.4 222.8 432 288 432s118.8-29.6 159.9-67.7C486.4 328.5 513 286 526.6 256c-13.6-30-40.2-72.5-78.6-108.3C406.8 109.6 353.2 80 288 80zM95.4 112.6C142.5 68.8 207.2 32 288 32s145.5 36.8 192.6 80.6c46.8 43.5 78.1 95.4 93 131.1c3.3 7.9 3.3 16.7 0 24.6c-14.9 35.7-46.2 87.7-93 131.1C433.5 443.2 368.8 480 288 480s-145.5-36.8-192.6-80.6C48.6 356 17.3 304 2.5 268.3c-3.3-7.9-3.3-16.7 0-24.6C17.3 208 48.6 156 95.4 112.6zM288 336c44.2 0 80-35.8 80-80s-35.8-80-80-80c-.7 0-1.3 0-2 0c1.3 5.1 2 10.5 2 16c0 35.3-28.7 64-64 64c-5.5 0-10.9-.7-16-2c0 .7 0 1.3 0 2c0 44.2 35.8 80 80 80zm0-208a128 128 0 1 1 0 256 128 128 0 1 1 0-256z" />
                     </svg>
+                    
+                </div>
+                <div class="error-message-password">
+                    <?php echo isset($error_message) ? '<svg class="iconoIncorrecte" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#000000}</style><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>' . $error_message  : ''; ?>
                 </div>
                 <input type="submit" class="boton-crear" value="Enviar"></input>
         </form>
