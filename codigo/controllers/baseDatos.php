dd77f2affe86d7d519022b6c478fe0ef2fb1941f
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2023-11-14 19:49:01 +0100

    arreglat problema visualitzar el titol sencer de la diapo pregunta Resposta:

--- codigo/controllers/baseDatos.php
+++ codigo/controllers/baseDatos.php
@@ -134,7 +134,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["anadirDiapositiva"])) 
         $dao->setPregunta($pregunta);
         $id_pregunta = $dao->getLastInsertId();
         $dao->setDiapositivesTitol($titol, $id_presentacio, $id_pregunta);
-        $dao->setDiapositivesTitol('Respuesta: ' . $titol, $id_presentacio, $id_pregunta);
+        $dao->setDiapositivesTitol($titol, $id_presentacio, $id_pregunta);
         $id_diapo = $dao->getLastInsertId();
         
         $respuestas = $_POST['opcion'];
